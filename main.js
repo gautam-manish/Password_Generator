6582affc3edfc4b3739934ec88c39a756b0cee38
Manish Gautam <manishgtm123@gmail.com>
2024-06-25 21:15:13 +0545

    Added JS code and made it functionable

--- main.js
+++ main.js
@@ -1,60 +1,60 @@
 
-const inputBox = document.getElementById("inputBox");
-const toCapital = document.getElementById("toCapital");
-const toSmall = document.getElementById("toSmall");
-const toNumber = document.getElementById("toNumber");
-const toSymbol = document.getElementById("toSymbol");
-const btn = document.getElementById("btn");
-const result = document.getElementById("result");
+let inputSlider = document.getElementById("inputSlider");
+let sliderValue = document.getElementById("sliderValue");
+let passBox = document.getElementById("passBox");
+let lowercase = document.getElementById("lowercase");
+let uppercase = document.getElementById("uppercase");
+let numbers = document.getElementById("numbers");
+let symbols = document.getElementById("symbols");
+let genBtn = document.getElementById("genBtn");
+let copyIcon = document.getElementById("copyIcon");
 
 
-btn.onclick = function () {
+// Showing input slider value
+sliderValue.textContent = inputSlider.value;
+inputSlider.addEventListener('input', () => {
+    sliderValue.textContent = inputSlider.value;
+});
 
-    function generatePassword(passwordLength, includeLowercase, includeUppercase, includeNumbers, includeSymbols) {
+genBtn.addEventListener('click', () => {
+    passBox.value = generatePassword();
+})
 
-        const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-        const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        const numberChars = "0123456789";
-        const symbolChars = "!@#$%&";
+let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let allNumbers = "0123456789";
+let allSymbols = "!@#$%^&*_"
+// Function to generate Password
+function generatePassword() {
+    let genPassword = "";
+    let allChars = "";
 
-        let allowedChars = "";
-        let password = "";
+    allChars += lowercase.checked ? lowerChars : "";
+    allChars += uppercase.checked ? upperChars : "";
+    allChars += numbers.checked ? allNumbers : "";
+    allChars += symbols.checked ? allSymbols : "";
 
-        allowedChars += includeLowercase ? lowercaseChars : "";
-        allowedChars += includeUppercase ? uppercaseChars : "";
-        allowedChars += includeSymbols ? symbolChars : "";
-        allowedChars += includeNumbers ? numberChars : "";
-
-        for (let i = 0; i < passwordLength; i++) {
-            const randomIndex = Math.floor(Math.random() * allowedChars.length);
-            password += allowedChars[randomIndex];
-        }
-
-        return password;
+    if (allChars == "" || allChars.length == 0) {
+        return genPassword;
     }
 
 
-    let passwordLength = inputBox.value;
-    let includeLowercase;
-    let includeUppercase;
-    let includeNumbers;
-    let includeSymbols;
-
-    if (toCapital.checked) {
-        includeUppercase = true;
-    }
-    if (toSmall.checked) {
-        includeLowercase = true;
-    }
-    if (toNumber.checked) {
-        includeNumbers = true;
-    }
-    if (toSymbol.checked) {
-        includeSymbols = true;
+    for (let i = 1; i <= inputSlider.value; i++) {
+        genPassword += allChars.charAt(Math.floor(Math.random() * allChars.length));
     }
 
-    const password = generatePassword(passwordLength, includeLowercase, includeUppercase, includeUppercase, includeSymbols, includeNumbers);
-
-    result.textContent = `${password}`;
+    return genPassword;
 }
 
+copyIcon.addEventListener('click', () => {
+    if (passBox.value != "" || passBox.value.lengh >= 1) {
+        navigator.clipboard.writeText(passBox.value);
+        copyIcon.innerText = "check";
+        copyIcon.title = "Password Copied";
+
+        setTimeout(() => {
+            copyIcon.innerHTML = "content_copy";
+            copyIcon.title = "";
+        }, 3000)
+    }
+});
